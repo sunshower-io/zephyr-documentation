5beb3495e52c80e06bda4896c220975adbbfcb2a
lisagumerman <lisajgumerman@gmail.com>
2019-12-02 11:52:01 -0700

    fixing config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,9 +1,10 @@
 module.exports = {
     title: 'Zephyr',
-    dest: './docs',
     description: 'A Java Plugin System for the 21st Century',
     themeConfig: {
         logo: 'wind-duotone.svg',
-        sidebar: 'auto'
+        sidebar: 'auto',
+        repo: 'sunshower-io/zephyr',
+        repoLabel: 'Get Zephyr',
     }
-}
+};
