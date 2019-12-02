3b717861a36a8271ea713e0734dcf71474fedb0b
Josiah Haswell <josiah.haswell@gmail.com>
2019-12-01 21:42:52 -0700

    Adding stuff

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,38 +4,39 @@ module.exports = {
     description: 'A Java Plugin System for the 21st Century',
     themeConfig: {
         logo: 'wind-duotone.svg',
-        sidebar: [
-            '/',
-            {
-                title: 'Concepts',
-                path: '/concepts/',
-                sidebarDepth: 2,
-                children: [
-                    ['/concepts/', 'Concepts'],
-                    ['/concepts/plugins-modules', 'Plugins and Modules'],
-                    ['/concepts/structure', 'Structure'],
-                    ['/concepts/lifecycle', 'Lifecycle']
-                ]
-            },
-            {
-                title: 'Getting Started',
-                path: '/get-started/',
-                sidebarDepth: 2,
-                children: [
-                    ['/get-started/', 'Installation'],
-                    ['/get-started/plugin', 'Writing Your First Plugin'],
-                    ['/get-started/command', 'Writing Your First Command']
-                ]
-            },
-            {
-                title: 'Available Plugins',
-                path: '/plugins/',
-                sidebarDepth: 2,
-                children: [
-                    ['/plugins/', 'Available Plugins'],
-                ]
-            }
-        ],
+        // sidebar: [
+        //     '/',
+        //     {
+        //         title: 'Concepts',
+        //         path: '/concepts/',
+        //         sidebarDepth: 2,
+        //         children: [
+        //             ['/concepts/', 'Concepts'],
+        //             ['/concepts/plugins-modules', 'Plugins and Modules'],
+        //             ['/concepts/structure', 'Structure'],
+        //             ['/concepts/lifecycle', 'Lifecycle']
+        //         ]
+        //     },
+        //     {
+        //         title: 'Getting Started',
+        //         path: '/get-started/',
+        //         sidebarDepth: 2,
+        //         children: [
+        //             ['/get-started/', 'Installation'],
+        //             ['/get-started/plugin', 'Writing Your First Plugin'],
+        //             ['/get-started/command', 'Writing Your First Command']
+        //         ]
+        //     },
+        //     {
+        //         title: 'Available Plugins',
+        //         path: '/plugins/',
+        //         sidebarDepth: 2,
+        //         children: [
+        //             ['/plugins/', 'Available Plugins'],
+        //         ]
+        //     }
+        // ],
+        sidebar: 'auto',
         repo: 'sunshower-io/zephyr',
         repoLabel: 'Get Zephyr',
         editLinks: false,
