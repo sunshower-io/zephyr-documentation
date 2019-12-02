529579741802e8c38077fc23d6643e3384c14767
Josiah Haswell <josiah.haswell@gmail.com>
2019-12-01 23:17:50 -0700

    Adding docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: 'Zephyr',
-    dest: 'docs/docs',
-    base: '/zephyr-documentation/',
+    dest: 'output',
+    base: '/output/',
     description: 'A Java Plugin System for the 21st Century',
     themeConfig: {
         logo: '/wind-duotone.svg',
