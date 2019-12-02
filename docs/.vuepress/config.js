cbcc8fc70e32e68bdc44fc9f72e2925771dc24a1
Josiah Haswell <josiah.haswell@gmail.com>
2019-12-01 23:22:33 -0700

    Adding stuff

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: 'Zephyr',
-    dest: 'output',
-    base: '/output/',
+    dest: 'en',
+    base: '/en/',
     description: 'A Java Plugin System for the 21st Century',
     themeConfig: {
         logo: '/wind-duotone.svg',
